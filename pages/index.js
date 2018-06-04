d06b26d5f5022802d4135c5f355d5fc2643ea8eb
Min Ji Lee <minjilee@mins-mbp.attlocal.net>
2018-06-04 08:56:20 -0700

    add clock

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import moment from 'moment';
+
+const today = moment();
+const formattedDate = today.format('YYYY MM DD hh:mm:ss');
+
 export default () => (
     <div>
-        Main page
+        Main page {formattedDate}
     </div>
 );
